2d475559eb5296eaa1669d4b99dc5bbf995c8e3f
c00kamunga <tsothalanti2@yahoo.com>
2020-08-21 16:47:23 -0700

    imported dependencies and created initial http method for testing server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const carsRouter = require('./carsRouter')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('<h1>Hello Everyone Welcome to Cookamunga</h1>')
+})
+
+server.use('./api/cars', carsRouter)
+
+module.exports = server
